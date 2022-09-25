d27de6a4e13a7ec5e9693b50692057c1cb0d9875
Rin Sah <pankot222@gmail.com>
2022-09-25 16:36:23 +0500

    small refact

--- gendiff/reader.py
+++ gendiff/reader.py
@@ -6,13 +6,15 @@ import yaml
 def read_path(file_path):
     extension = get_extension(file_path)
     if extension == '.json':
-        with open(file_path, 'r') as f:
-            res = json.load(f)
+        with open(file_path, 'r', encoding='utf-8') as file:
+            res = json.load(file)
             return res
     if extension in ('.yaml', '.yml'):
-        with open(file_path, 'r') as f:
-            res = yaml.safe_load(f)
+        with open(file_path, 'r', encoding='utf-8') as file:
+            res = yaml.safe_load(file)
             return res
+    else:
+        return None
 
 
 def get_extension(item: str):
