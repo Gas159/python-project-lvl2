0eb383fb0a060798e30b737eb381425be6c7c64f
Rin Sah <pankot222@gmail.com>
2022-10-03 14:12:26 +0500

    refact v2

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -2,38 +2,65 @@ def to_plain(diff_tree: dict) -> str:
     return make_plain(diff_tree)
 
 
-def make_plain(data: dict):
-    def inner(data, parent_key="") -> list:
-        lines = []
-        for elem in sorted(data.items()):
-            key, value = elem
-            if value.get('type') != 'unchanged':
+def make_plain(data: dict, parent_key=""):
+    lines = []
+    for elem in sorted(data.items()):
+        key, value = elem
+        if value.get('type') != 'unchanged':
 
-                value_of_type = value.get('type')
-                current_key = key
-                key = get_key(parent_key, current_key)
+            value_of_type = value.get('type')
+            current_key = key
+            key = get_key(parent_key, current_key)
 
-                if value_of_type == "added":
-                    lines.append(f"Property '{key}' was added with value:"
-                                 f" {to_str(value.get('value'))}")
+            if value_of_type == "added":
+                lines.append(f"Property '{key}' was added with value:"
+                             f" {to_str(value.get('value'))}")
 
-                elif value_of_type == "deleted":
-                    lines.append(f"Property '{key}' was removed")
+            elif value_of_type == "deleted":
+                lines.append(f"Property '{key}' was removed")
 
-                elif value_of_type == "changed":
-                    lines.append(f"Property '{key}' was updated. From "
-                                 f"{to_str(value.get('old'))} to "
-                                 f"{to_str(value.get('new'))}")
+            elif value_of_type == "changed":
+                lines.append(f"Property '{key}' was updated. From "
+                             f"{to_str(value.get('old'))} to "
+                             f"{to_str(value.get('new'))}")
 
-                elif value_of_type == "nested":
-                    nested_values = (inner(value.get('children'), key))
-                    [lines.append(i) for i in nested_values]
+            elif value_of_type == "nested":
+                nested_values = (make_plain(value.get('children'), key))
+                [lines.append(i) for i in nested_values]
 
-        return lines
-
-    return '\n'.join(inner(data))
+    return '\n'.join(lines)
 
 
+# def make_plain(data: dict):
+#     def inner(data, parent_key="") -> list:
+#         lines = []
+#         for elem in sorted(data.items()):
+#             key, value = elem
+#             if value.get('type') != 'unchanged':
+#
+#                 value_of_type = value.get('type')
+#                 current_key = key
+#                 key = get_key(parent_key, current_key)
+#
+#                 if value_of_type == "added":
+#                     lines.append(f"Property '{key}' was added with value:"
+#                                  f" {to_str(value.get('value'))}")
+#
+#                 elif value_of_type == "deleted":
+#                     lines.append(f"Property '{key}' was removed")
+#
+#                 elif value_of_type == "changed":
+#                     lines.append(f"Property '{key}' was updated. From "
+#                                  f"{to_str(value.get('old'))} to "
+#                                  f"{to_str(value.get('new'))}")
+#
+#                 elif value_of_type == "nested":
+#                     nested_values = (inner(value.get('children'), key))
+#                     [lines.append(i) for i in nested_values]
+#
+#         return lines
+#
+#     return '\n'.join(inner(data))
 def get_key(value, current_key):
     if value:
         return f"{value}.{current_key}"
